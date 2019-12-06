82ba0031f0317082ba75b6e344172f65c96ce86f
Adrian <adrianrc97@yahoo.es>
2019-12-06 20:31:52 +0100

    closes #18 Crear vista e controlador: CRUD de Issues

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,53 @@
+class IssuesController < ApplicationController
+  include SessionsModule
+
+  def show
+    @issue = acc.issues.find(params[:id])
+  end
+
+  def new
+    @device = acc.devices.find(params[:device_id])
+    @issue = @device.issues.build(:user => current_user, :date => Date.today)
+  end
+
+  def create
+    @device = acc.devices.find(params[:device_id])
+    @issue = @device.issues.build(issue_params)
+    @issue.user = current_user
+    if @issue.save
+      redirect_to device_issue_path(@device, @issue)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @device = acc.devices.find(params[:device_id])
+    @issue = @device.issues.find(params[:id])
+  end
+
+  def update
+    @device = acc.devices.find(params[:device_id])
+    @issue = @device.issues.find(params[:id])
+    respond_to do |format|
+      if @issue.update(issue_params)
+        format.html { redirect_to device_issue_path(@device, @issue) }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @device = acc.devices.find(params[:device_id])
+    @issue = @device.issues.find(params[:id])
+    @issue.trash!
+    redirect_to device_path(@device)
+  end
+
+  private
+  def issue_params
+    params.require(:issue).permit(:title, :description, :date, :state, :priority)
+  end
+
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,26 @@ module ApplicationHelper
     end
   end
 
+  # Xenera elemento que mostra a prioridade das issues e aplica un estilo diferente
+  # segundo o tipo de prioridade
+  def priority_info_html(priority)
+    priority_translated = I18n.t("global_constants.issue_priorities.#{priority}")
+    class_to_content = case priority
+    when ISSUE_PRIORITIES[:low]
+      'text-secondary'
+    when ISSUE_PRIORITIES[:normal]
+      'text-info'
+    when ISSUE_PRIORITIES[:high]
+      'text-danger'
+    end
+    content_tag(:small, priority_translated, :class => class_to_content)
+  end
+
+  # Devolve a clase para as issues do show de dispositivo
+  def class_for_issue_by_state(issue)
+    klass = "list-group-item list-group-item-action list-group-item-dark"
+    klass << " active" if issue.open? || issue.in_progress?
+    klass
+  end
+
 end
